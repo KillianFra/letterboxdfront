7d40971930ee7609f19552353a9383056140499a
KillianFra <110053182+KillianFra@users.noreply.github.com>
2025-02-15 17:08:25 +0100

    feat(actions): added the deployment to Railway

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir: ".next",
   reactStrictMode: true,
   async rewrites() {
     return [
